5eac73371c89142ad54d3ab2bc747fce700012e7
metaory <metaory@gmail.com>
2025-06-14 23:00:54 +0330

    0614_2300

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,13 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        pad: 'pad.html'
+      }
+    }
   }
 }) 
